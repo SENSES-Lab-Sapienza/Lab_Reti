d726271b5e31f5f0b8427d40b3090d48c58f3ad9
Michele Mastrogiovanni <michele.mastrogiovanni@elerian.ai>
2024-03-05 23:27:50 +0100

    added minidatabase homework

---
+++ Homework/minidatabase/database.h
@@ -0,0 +1,39 @@
+
+// This represent a record in the only schema of this database
+struct Persona {
+    char name[20];
+    char surname[50];
+    char address[100];
+    int age;
+};
+
+// This is a node of an index that hold a string
+typedef struct IndexNodeString {
+    char * value;
+    struct IndexNodeString * left;
+    struct IndexNodeString * right;
+} IndexNodeString;
+
+// This is a node of an index that hold an int
+typedef struct IndexNodeInt {
+    int value;
+    struct IndexNodeInt * left;
+    struct IndexNodeInt * right;
+} IndexNodeInt;
+
+// A database hold a set of records and a set of indexes
+typedef struct {
+    IndexNodeString * name;
+    IndexNodeString * surname;
+    IndexNodeString * address;
+    IndexNodeInt * age;
+} Database;
+
+// TODO implement the following methods
+// The method return a Persona or NULL 
+
+void insert(Database * database, Persona * persona);
+Persona* findByName(Database * database, char * name);
+Persona* findBySurname(Database * database, char * surname);
+Persona* findByAddress(Database * database, char * address);
+Persona* findByAge(Database * database, int age);
