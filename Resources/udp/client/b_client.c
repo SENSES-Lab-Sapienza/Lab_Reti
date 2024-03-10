73019fb51d3e11e9191145b7feea65c6e87654e6
Michele Mastrogiovanni <michele.mastrogiovanni@elerian.ai>
2024-03-10 23:25:36 +0100

    fix: cleanup

--- Resources/udp/client/b_client.c
+++ Resources/udp/client/b_client.c
@@ -13,8 +13,6 @@
 
 int main(int argc, char * argv[])
 {
-    // int PORT = atoi(argv[1]);
-
     int sock;
     int yes = 1;
     struct sockaddr_in broadcast_addr;
@@ -26,12 +24,17 @@ int main(int argc, char * argv[])
     char buffer[1024];
     int i;
 
+    setvbuf(stdout, NULL, _IONBF, 0); 
+
+    printf("Inizializing...\n");
+
     sock = socket(AF_INET, SOCK_DGRAM, 0);
     if (sock < 0)
     {
         perror("sock error");
         return -1;
     }
+
     ret = setsockopt(sock, SOL_SOCKET, SO_BROADCAST, (char *)&yes, sizeof(yes));
     if (ret == -1)
     {
@@ -41,6 +44,8 @@ int main(int argc, char * argv[])
 
     addr_len = sizeof(struct sockaddr_in);
 
+    printf("Prepare broadcast message\n");
+
     memset((void *)&broadcast_addr, 0, addr_len);
     broadcast_addr.sin_family = AF_INET;
     broadcast_addr.sin_addr.s_addr = htonl(INADDR_BROADCAST);
@@ -48,22 +53,24 @@ int main(int argc, char * argv[])
 
     for (i = 0; i < 3; i++)
     {
-        ret = sendto(sock, IP_FOUND, strlen(IP_FOUND), 0, (struct sockaddr *)&broadcast_addr, addr_len);
+        printf("Sending %s in broadcast\n", IP_FOUND);
+        ret = sendto(sock, IP_FOUND, strlen(IP_FOUND)+1, 0, (struct sockaddr *)&broadcast_addr, addr_len);
 
         FD_ZERO(&readfd);
         FD_SET(sock, &readfd);
 
+        printf("Wait message back\n");
         ret = select(sock + 1, &readfd, NULL, NULL, NULL);
 
         if (ret > 0)
         {
             if (FD_ISSET(sock, &readfd))
             {
-                count = recvfrom(sock, buffer, 1024, 0, (struct sockaddr *)&server_addr, &addr_len);
-                printf("\trecvmsg is %s\n", buffer);
+                count = recvfrom(sock, buffer, 1024, 0, (struct sockaddr *)&server_addr, (socklen_t*) &addr_len);
+                printf("Received %s\n", buffer);
                 if (strstr(buffer, IP_FOUND_ACK))
                 {
-                    printf("\tfound server IP is %s, Port is %d\n", inet_ntoa(server_addr.sin_addr), htons(server_addr.sin_port));
+                    printf("Found server IP is %s, Port is %d\n", inet_ntoa(server_addr.sin_addr), htons(server_addr.sin_port));
                 }
             }
         }
--- Resources/udp/server/b_server.c
+++ Resources/udp/server/b_server.c
@@ -1,5 +1,5 @@
-#include <netinet/in.h>
 #include <stdio.h>
+#include <netinet/in.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
@@ -9,12 +9,10 @@
 
 #define IP_FOUND "IP_FOUND"
 #define IP_FOUND_ACK "IP_FOUND_ACK"
-#define PORT 9998
+#define PORT 9999
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
-    // int PORT = atoi(argv[1]);
-
     int sock;
     int yes = 1;
     struct sockaddr_in client_addr;
@@ -24,6 +22,10 @@ int main(int argc, char * argv[])
     int ret;
     fd_set readfd;
     char buffer[1024];
+
+    setvbuf(stdout, NULL, _IONBF, 0); 
+
+    printf("Inizializing...\n");
 
     sock = socket(AF_INET, SOCK_DGRAM, 0);
     if (sock < 0)
@@ -47,22 +49,27 @@ int main(int argc, char * argv[])
     }
     while (1)
     {
+        printf("Server started\n");
         FD_ZERO(&readfd);
         FD_SET(sock, &readfd);
+
+        printf("Waiting connections\n");
 
         ret = select(sock + 1, &readfd, NULL, NULL, 0);
         if (ret > 0)
         {
             if (FD_ISSET(sock, &readfd))
             {
-                count = recvfrom(sock, buffer, 1024, 0, (struct sockaddr *)&client_addr, &addr_len);
+                count = recvfrom(sock, buffer, 1024, 0, (struct sockaddr *)&client_addr, (socklen_t*) &addr_len);
+                printf("Message received: %s\n", buffer);
                 if (strstr(buffer, IP_FOUND))
                 {
-                    printf("\nClient connection information:\n\t IP: %s, Port: %d\n",
+                    printf("Client connection information:\n\t IP: %s, Port: %d\n",
                            inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 
                     memcpy(buffer, IP_FOUND_ACK, strlen(IP_FOUND_ACK) + 1);
-                    count = sendto(sock, buffer, strlen(buffer), 0, (struct sockaddr *)&client_addr, addr_len);
+                    printf("Sending %s message back to client\n", IP_FOUND_ACK);
+                    count = sendto(sock, buffer, strlen(buffer) + 1, 0, (struct sockaddr *)&client_addr, addr_len);
                 }
             }
         }
