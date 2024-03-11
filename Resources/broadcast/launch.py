43b3383495b6d93d62025020ba097ea0cf51abac
Michele Mastrogiovanni <michele.mastrogiovanni@elerian.ai>
2024-03-11 23:06:11 +0100

    added launcher for a random topology of nodes

---
+++ Resources/broadcast/launch.py
@@ -0,0 +1,67 @@
+import random
+import subprocess
+
+num_nodes = 10
+
+# Return a random connected graph n nodes and n-1 edges 
+def get_random_edges(num_nodes):
+
+    partition = dict((i, [i]) for i in range(num_nodes))
+
+    def pair_random(max):
+        a = random.randint(0, max-1)
+        while True:
+            b = random.randint(0, max-1)
+            if a != b:
+                return (a, b) if a < b else (b, a)
+            
+    edges = []
+
+    while len(partition.keys()) > 1:
+        keys = list(partition.keys())
+        a, b = pair_random(len(keys))
+        a = keys[a]
+        b = keys[b]
+        edges.append((partition[a][0], partition[b][0]))
+        partition[a] = partition[a] + partition[b]
+        del partition[b]
+
+    return edges
+
+# Get yaml file for a n equal containers connected 
+# according a specific set of edges
+def get_yaml(num_nodes, edges):
+    buffer = "services:\n"
+    for node in range(num_nodes):
+        buffer += f"\tnode{node}:\n"
+        buffer += "\t\tbuild:\n"
+        buffer += "\t\t\tcontext: ../udp/client\n"
+        buffer += "\t\t\tdockerfile: Dockerfile\n"
+        buffer += "\t\tnetworks:\n"
+        for edge in edges:
+            if node in edge:
+                buffer += f"\t\t- edge-{edge[0]}-{edge[1]}\n"
+    buffer += "networks:\n"
+    for edge in edges:
+        buffer += f"\tedge-{edge[0]}-{edge[1]}:\n"
+
+    buffer = buffer.replace("\t", "  ")
+    return buffer
+
+with open("compose.yaml", "w+") as file:
+    file.write(get_yaml(num_nodes, get_random_edges(num_nodes)))
+
+cmd = "docker compose up --build"
+
+result = subprocess.run(cmd.split(" "), stderr=subprocess.PIPE)
+
+print(result.stderr.decode('utf-8'))
+
+
+
+
+
+
+
+
+
